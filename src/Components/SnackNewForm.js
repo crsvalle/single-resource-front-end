de3bff99c76e2485e57698e34d53840efe61a9ad
crsvalle <cristian.valle@pursuit.org>
2023-08-07 20:08:15 -0400

    fixed edit/new

--- src/Components/SnackNewForm.js
+++ src/Components/SnackNewForm.js
@@ -21,7 +21,12 @@ function SnackNewForm() {
 
   const [snack, setSnack] = useState({
     name: "",
-    url: "",
+    image: "",
+    serving: 0,
+    protein: 0,
+    sugar: 0,
+    sodium: 0,
+    fat: 0,
     type: "",
     is_favorite: false,
   });
@@ -50,25 +55,69 @@ function SnackNewForm() {
           placeholder="Name of Snack"
           required
         />
-        <label htmlFor="url">URL:</label>
+        <label htmlFor="image">Image:</label>
         <input
-          id="url"
+          id="image"
           type="text"
           pattern="http[s]*://.+"
           required
-          value={snack.url}
+          value={snack.image}
           placeholder="http://"
           onChange={handleTextChange}
         />
-        <label htmlFor="type">Type:</label>
+        <label htmlFor="serving">Serving Size(g):</label>
         <input
+          id="serving"
+          type="number"
+          value={snack.serving}
+          placeholder="0g"
+          onChange={handleTextChange}
+        />
+        <label htmlFor="protein"> Protein(g):</label>
+        <input
+          id="protein"
+          type="number"
+          value={snack.protein}
+          placeholder="0g"
+          onChange={handleTextChange}
+        />
+        <label htmlFor="sugar">Sugar(g):</label>
+        <input
+          id="sugar"
+          type="number"
+          value={snack.sugar}
+          placeholder="0g"
+          onChange={handleTextChange}
+        />
+        <label htmlFor="sodium">Sodium(mg):</label>
+        <input
+          id="sodium"
+          type="text"
+          value={snack.sodium}
+          placeholder="0g"
+          onChange={handleTextChange}
+        />
+        <label htmlFor="fat">Fat(g):</label>
+        <input
+          id="fat"
+          type="text"
+          value={snack.fat}
+          placeholder="0g"
+          onChange={handleTextChange}
+        />
+        <label htmlFor="type">Type:</label>
+        <select
           id="type"
           type="text"
           name="type"
           value={snack.type}
           placeholder="healthy, tasty, ..."
           onChange={handleTextChange}
-        />
+        >
+          <option value=""></option>
+          <option value="Savory">Savory</option>
+          <option value="Sweet">Sweet</option>
+        </select>
         <label htmlFor="is_favorite">Favorite:</label>
         <input
           id="is_favorite"
